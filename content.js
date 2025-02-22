6e567f82b45111d009d8898ad4f0903057e8d754
rboddy <avidryanb@gmail.com>
2025-02-21 23:31:09 -0500

    Canvas can be toggled, and is now just a yellow border

--- content.js
+++ content.js
@@ -1,12 +1,22 @@
 // This script adds in a canvas element that overlays the entire page
-let canvas = document.createElement("canvas");
-canvas.id = "overlayCanvas";
-canvas.style.width = "100%";
-canvas.style.height = "100vh";
-canvas.style.position = "fixed";
-canvas.style.top = "0";
-canvas.style.left = "0";
-canvas.style.backgroundColor = "red";
-canvas.style.opacity = ".5";
-canvas.style.zIndex = "1000";
-document.body.appendChild(canvas);
+
+// Check if the canvas exists and add it. Otherwise, remove it.
+if (document.getElementById("overlayCanvas") === null) {
+  // Create the canvas
+  let canvas = document.createElement("canvas");
+
+  // Style the canvas
+  canvas.id = "overlayCanvas";
+  canvas.style.width = "100%";
+  canvas.style.height = "100vh";
+  canvas.style.position = "fixed";
+  canvas.style.top = "0";
+  canvas.style.left = "0";
+  canvas.style.zIndex = "1000";
+  canvas.style.border = "5px dashed yellow";
+
+  document.body.appendChild(canvas);
+}
+else {
+  document.body.removeChild(document.getElementById("overlayCanvas"));
+}
