d6be60f58798309f19df7a4fb113cdeffd4ca56e
rboddy <rboddy@redventures.com>
2025-03-06 10:37:04 -0500

    Persistence! At last

--- persist.js
+++ persist.js
@@ -23,95 +23,54 @@ function spawnPersistedCanvas(dataURL) {
 
   // Add the canvas
   document.body.appendChild(canvas);
-  let ctx = canvas.getContext("2d");
 
   let img = new Image();
   img.src = dataURL;
-  img.onload = () => {
-    console.log(img);
-    ctx.drawImage(img, 0, 0);
-  };
-
-  // Create an image uploader modal
-  let uploadModal = document.createElement("dialog");
-  uploadModal.id = "uploadModal";
-  uploadModal.style.position = "fixed";
-  uploadModal.style.top = "10%";
-  uploadModal.style.border = "none";
-  uploadModal.style.width = "500px";
-  uploadModal.style.borderRadius = "15px";
-  uploadModal.style.boxShadow =
-    "box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1)";
-
-  // Create the image uploader input
-  let imageInput = document.createElement("input");
-
-  imageInput.type = "file";
-  imageInput.id = "imageLoader";
-  imageInput.position = "absolute";
-  imageInput.accept = ".png, .jpg, .jpeg, .svg";
-  imageInput.addEventListener("change", handleImage, false);
-
-  // Add the input to the modal
-  uploadModal.appendChild(imageInput);
-
-  // Add the modal to the document
-  document.body.appendChild(uploadModal);
-}
-
-function handleImage(e) {
-  let canvas = document.getElementById("overlayCanvas");
   let ctx = canvas.getContext("2d");
   let isDragging = false;
   let startX = 0;
   let startY = 0;
   let imageX = 0;
   let imageY = 0;
+  img.onload = () => {
+    console.log(img);
+    ctx.drawImage(img, 0, 0);
 
-  let reader = new FileReader();
-  reader.onload = function (event) {
-    let img = new Image();
-    img.onload = () => {
-      // Draw the image initially
+    canvas.addEventListener("mousedown", (event) => {
+      isDragging = true;
+      startX = event.clientX - canvas.offsetLeft;
+      startY = event.clientY - canvas.offsetTop;
+    });
 
-      ctx.drawImage(img, imageX, imageY);
+    canvas.addEventListener("mousemove", (event) => {
+      if (isDragging) {
+        const deltaX = event.clientX - canvas.offsetLeft - startX;
 
-      canvas.addEventListener("mousedown", (event) => {
-        isDragging = true;
+        const deltaY = event.clientY - canvas.offsetTop - startY;
+
+        imageX += deltaX;
+
+        imageY += deltaY;
+
+        // Clear canvas and redraw the image at new position
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        ctx.drawImage(img, imageX, imageY);
+
         startX = event.clientX - canvas.offsetLeft;
+
         startY = event.clientY - canvas.offsetTop;
-      });
+      }
+    });
 
-      canvas.addEventListener("mousemove", (event) => {
-        if (isDragging) {
-          const deltaX = event.clientX - canvas.offsetLeft - startX;
-
-          const deltaY = event.clientY - canvas.offsetTop - startY;
-
-          imageX += deltaX;
-
-          imageY += deltaY;
-
-          // Clear canvas and redraw the image at new position
-
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-          ctx.drawImage(img, imageX, imageY);
-
-          startX = event.clientX - canvas.offsetLeft;
-
-          startY = event.clientY - canvas.offsetTop;
-        }
-      });
-
-      canvas.addEventListener("mouseup", () => {
-        isDragging = false;
-      });
-    };
-    img.src = event.target.result;
+    canvas.addEventListener("mouseup", () => {
+      isDragging = false;
+      saveCanvas(canvas);
+    });
   };
-  reader.readAsDataURL(e.target.files[0]);
+}
 
-  document.getElementById("imageLoader").value = "";
-  document.getElementById("uploadModal").close();
+function saveCanvas(canvas) {
+  localStorage.setItem("storedEasel", canvas.toDataURL());
 }
